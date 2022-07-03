58d46cf35f36e2590f3d73b48de02a982dffee33
Isabella <isabella_darby@yahoo.com>
2022-07-02 21:14:27 -0500

    school summary complete.

---
+++ resources/New folder/Python_Practice.py
@@ -0,0 +1,12 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+
+for i in range(len(counties)):
+    print(counties[i])
+    voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+    print(county_dict)
+for county, voters in counties_dict.items():
+        print(f"{county} county has {voters} registered voters.")
